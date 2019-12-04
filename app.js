ad2860911a13fb4c4e309c557b6d37c40cfd071a
Alex <Alexnguyen@coursera.com>
2019-12-04 15:25:50 +0700

    webapp

--- app.js
+++ app.js
@@ -2,20 +2,38 @@
   'use strict';
   angular.module('webapp',[])
   .controller('webappcontroller',webappcontroller);
-  webappcontroller.$inject=['$scope'];
-  function webappcontroller($scope){
-    $scope.onceCounter=0;
+  webappcontroller.$inject=['$scope','$timeout'];
+  function webappcontroller($scope,$timeout){
     $scope.counter = 0;
     $scope.counter1 = 0;
     $scope.shownumberofwatcher = function(){
       console.log("# of watchers:",$scope.$$watchersCount);
     };
+    // Use timeout with inject $timeout for timer
+    // $scope.upCounter = function(){
+    //   $timeout(function(){
+    //     $scope.counter++;
+    //     console.log("counter incremented!");
+    //   },2000)
+    // };
+    // use apply or digest for timeout
+    // $scope.upCounter = function(){
+    //   $timeout(function(){
+    //     $scope.counter++;
+    //     console.log("counter incremented!");
+    //   },2000)
+    // };
     $scope.upCounter = function(){
-      $scope.counter++;
+      $timeout(function(){
+        $scope.$apply(function(){
+          $scope.counter++;
+          console.log("counter incremented!");
+        });
+      },2000);
     };
-    $scope.upCounter1 = function(){
-      $scope.counter1++;
-    };
+    // $scope.upCounter1 = function(){
+    //   $scope.counter1++;
+    // };
     $scope.$watch(function(){
       console.log("Digest !!!!!");
     });
