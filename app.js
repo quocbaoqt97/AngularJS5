b5f1048e664d667a65272ad005aa1a4a4d7938d9
Alex <Alexnguyen@coursera.com>
2019-12-04 11:23:25 +0700

    webapp

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-(Function(){
+( function () {
+  'use strict';
   angular.module('webapp',[])
   .controller('webappcontroller',webappcontroller);
+  webappcontroller.$inject=['$scope'];
+  function webappcontroller($scope){
+    $scope.onceCounter=0;
+    $scope.counter = 0;
+    $scope.counter1 = 0;
+    $scope.shownumberofwatcher = function(){
+      console.log("# of watchers:",$scope.$$watchersCount);
+    };
+    $scope.upCounter = function(){
+      $scope.counter++;
+    };
+    $scope.upCounter1 = function(){
+      $scope.counter1++;
+    };
+    $scope.$watch('counter',function(newvalue,oldvalue){
+      console.log("old value:",oldvalue);
+      console.log("new value:",newvalue);
+    });
+    $scope.$watch('counter1',function(newvalue,oldvalue){
+      console.log("old value:",oldvalue);
+      console.log("new value:",newvalue);
+    });
+  }
 
-})()
+})();
